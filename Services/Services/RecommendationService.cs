12a96a7ebbbf0d52dd2f89b915959bbb14cdf881
Ruzin413 <rzmdr413@gmail.com>
2025-08-15 07:05:32 +0545

    updated recommendation algorithm

--- Services/Services/RecommendationService.cs
+++ Services/Services/RecommendationService.cs
@@ -3,7 +3,6 @@ using Repository.Repository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Services.Services
@@ -23,25 +22,45 @@ namespace Services.Services
             {
                 return new List<TourPackage>();
             }
+
             int latestBookedPackageId = latestPayment.Tourpackageid;
             float userLat = latestPayment.Latitude;
             float userLong = latestPayment.Longitude;
+
             var allTourPackages = await _repository.GetAllTourPackagesAsync();
-            var recommendedPackagesWithDistance = allTourPackages
-                .Where(tp => tp.TourPackageId != latestBookedPackageId) 
-                .Select(tp => new 
+
+
+            var packagesWithDistanceAndRating = allTourPackages
+                .Where(tp => tp.TourPackageId != latestBookedPackageId)
+                .Select(tp => new
                 {
                     Package = tp,
-                    Distance = CalculateDistance(userLat, userLong, tp.Lat, tp.Long)
+                    Distance = CalculateDistance(userLat, userLong, tp.Lat, tp.Long),
+                    Rating = tp.Rating
                 })
-                .ToList(); 
-            var sortedRecommendations = recommendedPackagesWithDistance
-                .OrderBy(x => x.Distance)
-                .ThenByDescending(x => x.Package.Rating) 
                 .ToList();
-            var topRecommendations = sortedRecommendations.Take(5).Select(x => x.Package).ToList();
-            return topRecommendations;
+
+            double maxDistance = packagesWithDistanceAndRating.Max(x => x.Distance);
+            double maxRating = 5.0; 
+
+            // Weights
+            double weightLocation = 0.6; 
+            double weightRating = 0.4;   
+            var scoredPackages = packagesWithDistanceAndRating
+                .Select(x => new
+                {
+                    Package = x.Package,
+                    Score = (weightLocation * (1 - (x.Distance / maxDistance))) +
+                            (weightRating * (x.Rating / maxRating))
+                })
+                .OrderByDescending(x => x.Score)
+                .Take(5)
+                .Select(x => x.Package)
+                .ToList();
+
+            return scoredPackages;
         }
+
         private double CalculateDistance(float lat1, float lon1, float lat2, float lon2)
         {
             const double R = 6371e3; 
@@ -49,9 +68,11 @@ namespace Services.Services
             var lat2Rad = lat2 * Math.PI / 180;
             var deltaLat = (lat2 - lat1) * Math.PI / 180;
             var deltaLon = (lon2 - lon1) * Math.PI / 180;
+
             var a = Math.Sin(deltaLat / 2) * Math.Sin(deltaLat / 2) +
                     Math.Cos(lat1Rad) * Math.Cos(lat2Rad) *
                     Math.Sin(deltaLon / 2) * Math.Sin(deltaLon / 2);
+
             var c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
             return R * c / 1000; 
         }
